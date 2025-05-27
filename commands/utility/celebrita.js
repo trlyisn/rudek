8494328b0733ac057b8ac81b360f69165d459a36
trlyisn <trlyisn@mail.com>
2025-05-27 15:36:29 +0200

    Add user ID check and response message in celebrita command

--- commands/utility/celebrita.js
+++ commands/utility/celebrita.js
@@ -6,8 +6,20 @@ module.exports = {
     .setName("celebrita")
     .setDescription("KDD CS PLISSS"),
   async execute(interaction) {
+    if (interaction.user.id !== process.env.pikabooID) {
+      return interaction.reply({
+        content: "jdi do prdele, ty zkurvenej curaku",
+        ephemeral: true,
+      });
+    }
+
     for (let i = 0; i < 10; i++) {
       await interaction.channel.send(`<@!${process.env.kd1ID}>`);
     }
+
+    await interaction.reply({
+      content: "KDD CS PLISSS",
+      ephemeral: true,
+    });
   },
 };
