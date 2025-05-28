fb358ea6b29f54cadff5274109022461bb303f9c
trlyisn <trlyisn@mail.com>
2025-05-28 07:27:49 +0200

    Fix typo in Slot command embed send method

--- commands/gambling/Slot.js
+++ commands/gambling/Slot.js
@@ -22,6 +22,6 @@ module.exports = {
         text: isWin ? "Ses dobrej, vyhrals" : "ty pico zkurvena, prohrals",
       });
 
-    channel.send({ embeds: [exampleEmbed] });
+    channel.send({ embeds: [resultEmbed] });
   },
 };
